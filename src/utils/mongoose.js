dba208beb60791799fd840f9d01df0a952fb0cda
Gibran <giggs_98@outlook.com>
2022-08-24 18:59:41 -0500

    Se añadio la cadena al cluster

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/Products")
+mongoose.connect("mongodb://dbo-operator:mongo0p3rqt0r@Mongo01:27017/Products?replicaSet=dbrs")
     .then(() => console.log("Connected to Mongo Database"))
     .catch(err => console.error(err));
